46c150d47032262df264b1aeb8c2c07c9ccf385d
连志明 <lianzhiming@beisen.com>
2019-05-27 19:07:21 +0800

    初次提交webpack的react配置

--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,28 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
+                use: [
+                    {
+                        loader: "babel-loader"
+                    }
+                ]
             },
             {
                 test: /\.html$/,
                 use: [
-                  {
-                    loader: "html-loader"
-                  }
+                    {
+                        loader: "html-loader"
+                    }
                 ]
             }
         ]
     },
     plugins: [
         new HtmlWebPackPlugin({
-          template: "./src/index.html",
-          filename: "./index.html"
+            template: "./src/index.html",
+            filename: "./index.html"
         })
     ]
 }
