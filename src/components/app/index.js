943bf892dfe61dc16c7d9219d352e9a780dfd1a2
连志明 <lianzhiming@beisen.com>
2019-06-06 19:09:22 +0800

    111

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,41 +1,69 @@
 import React, { Component } from 'react';
-import {ThemeContext, themes} from './theme-context';
+import { ThemeContext, themes } from './theme-context';
 import ThemedButton from './themed-button';
-import { Button } from 'antd';
+import { Drawer, Button } from 'antd';
+//import { Resizable, ResizableBox } from 'react-resizable';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          theme: themes.ZH_CN,
-        };
-    
-        this.toggleTheme = () => {
-          this.setState(state => ({
-            theme:
-              state.theme === themes.ZH_CN
-                ? themes.EN_US
-                : themes.ZH_CN,
-          }));
-        };
-    }
-    render() {
-        let {
-            theme
-        }=this.state;
-        return (
-            <div className="app">
-               app
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: themes.ZH_CN,
+      visible: false
+    };
+
+    this.toggleTheme = () => {
+      this.setState(state => ({
+        theme:
+          state.theme === themes.ZH_CN
+            ? themes.EN_US
+            : themes.ZH_CN,
+      }));
+    };
+  }
+  render() {
+    let {
+      theme,
+      visible
+    } = this.state;
+    return (
+      <div className="app">
+        app
                {console.log(this.state.theme)}
-                <ThemeContext.Provider value={theme}>
-                    <ThemedButton onClick={this.toggleTheme}>
-                       {theme.components.app.buttonTitle}
-                    </ThemedButton>
-                    <Button type="primary">Primary</Button>
-                </ThemeContext.Provider>
-            </div>
-        );
-    }
+        <ThemeContext.Provider value={theme}>
+          <ThemedButton onClick={this.toggleTheme}>
+            {theme.components.app.buttonTitle}
+          </ThemedButton>
+          <Button type="primary" onClick={this.showDrawer}>Open</Button>
+          {/* <ResizableBox width={200} height={200} draggableOpts={{ ...}}
+            minConstraints={[100, 100]} maxConstraints={[300, 300]}>
+            <span>Contents</span>
+          </ResizableBox> */}
+        </ThemeContext.Provider>
+        <Drawer
+          title="Basic Drawer"
+          placement="right"
+          closable={false}
+          onClose={this.onClose}
+          visible={visible}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Drawer>
+      </div>
+    );
+  }
+  showDrawer = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+  onClose = () => {
+    this.setState({
+      visible: false,
+    });
+  };
 }
 
 export default App;
